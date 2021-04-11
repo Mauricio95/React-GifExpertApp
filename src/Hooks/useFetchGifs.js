5a289b4a3c7496a8360efe56e3f247c9bfff5987
mauricio.melo <mauricio.melo.glz@hotmail.com>
2021-04-11 14:17:40 -0500

    First commit

---
+++ src/Hooks/useFetchGifs.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { getGifts } from "../Helpers/getGifts";
+export const useFetchGifs = ( category ) => {
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect ( () => {
+        getGifts(category)
+        .then(imgs => {
+
+            setState({
+                data: imgs,
+                loading: false
+            }) 
+
+            
+        });
+    }, [category])
+
+
+    return state;
+}
